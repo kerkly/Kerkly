4450e909c9f348740500ae40f18a632d6c6f50ff
Luis Alfredo <kerklyk@gmail.com>
2023-04-28 09:50:28 -0600

    hola mundo

--- app/src/main/java/com/example/kerklytv2/url/Url.kt
+++ app/src/main/java/com/example/kerklytv2/url/Url.kt
@@ -3,6 +3,6 @@ package com.example.kerklytv2.url
 class Url {
     val URL: String = "https://www.saua.uagro.mx/kerkly"
   //val URL: String = "https://6b4c-201-148-25-74.ngrok-free.app/kerkly/kerkly"
-
+val otro : String ="hoola maundo"
 
 }
