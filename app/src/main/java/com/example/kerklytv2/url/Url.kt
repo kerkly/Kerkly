549f383a53102d9fce24b0b8df4fd3a04ce59788
Luis Alfredo <kerklyk@gmail.com>
2023-08-03 13:39:49 -0600

    actualizacion  03 de agosto

--- app/src/main/java/com/example/kerklytv2/url/Url.kt
+++ app/src/main/java/com/example/kerklytv2/url/Url.kt
@@ -2,6 +2,6 @@ package com.example.kerklytv2.url
 
 class Url {
    //val URL: String = "https://www.saua.uagro.mx/kerkly"
-  val URL: String = "https://c688-201-148-25-74.ngrok-free.app/Web-services---Kerkly/kerkly"
+  val URL: String = "https://c187-201-148-25-74.ngrok-free.app/Web-services---Kerkly/kerkly"
 
 }
