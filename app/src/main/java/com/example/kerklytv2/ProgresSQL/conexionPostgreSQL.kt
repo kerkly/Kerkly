8f5f85c7905b783569810552d66c0f8dea576d51
kerkly <kerklyk@gmail.com>
2023-11-24 14:26:13 -0600

    24 nov 2023

--- app/src/main/java/com/example/kerklytv2/ProgresSQL/conexionPostgreSQL.kt
+++ app/src/main/java/com/example/kerklytv2/ProgresSQL/conexionPostgreSQL.kt
@@ -29,8 +29,8 @@ class conexionPostgreSQL {
         }
         try {
             Class.forName("org.postgresql.Driver") // Cargar el driver JDBC
-            val host = "0.tcp.ngrok.io"
-            val port = "15897"
+            val host = "2.tcp.ngrok.io"
+            val port = "14955"
             val databaseName = "Kerkly"
             val username = "luis_admin"
             val password = "Lu0599@"
--- app/src/main/java/com/example/kerklytv2/vista/MainActivity.kt
+++ app/src/main/java/com/example/kerklytv2/vista/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.kerklytv2.vista
 
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kerklytv2.R
 import com.example.kerklytv2.controlador.MainActiityControlador
@@ -23,6 +27,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var controlador: MainActiityControlador
     private lateinit var id: String
 
+
     @SuppressLint("HardwareIds")
     override fun onCreate(savedInstanceState: Bundle?) {
         controlador = MainActiityControlador()
@@ -35,6 +40,23 @@ class MainActivity : AppCompatActivity() {
         layoutUsuario = findViewById(R.id.textnputUser)
         layoutContra = findViewById(R.id.textnputPassword)
         kerkly = Kerkly()
+
+        val ButtonactualizarAPP = findViewById<Button>(R.id.buttonActualizarAPP)
+        ButtonactualizarAPP.setOnClickListener {
+            // URL de Google Drive que deseas abrir
+            val urlGoogleDrive = "https://drive.google.com/file/d/1WuyI1UFpvgaIGtrIcXHvQNu3rW3Zaa5O/view?usp=drive_link"
+            // Crear un Intent con la acción ACTION_VIEW y la URL
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(urlGoogleDrive))
+
+            // Verificar si hay una aplicación para manejar la acción VIEW (navegador)
+            if (intent.resolveActivity(packageManager) != null) {
+                // Abrir la URL en el navegador
+                startActivity(intent)
+            } else {
+                // Manejar el caso en el que no haya una aplicación para manejar la acción VIEW
+                Toast.makeText(this, "No se encontró una aplicación para abrir la URL", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     fun entrar(view: View) {
